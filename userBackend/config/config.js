c66bd25995d3b19082a5ba76187a88b93df497a5
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-12-28 18:41:35 +0300

    ss

--- userBackend/config/config.js
+++ userBackend/config/config.js
@@ -7,8 +7,12 @@ module.exports = {
 		host: process.env.DB_HOST,
 		dialect: process.env.DB_DIALECT,
 		port: process.env.DB_PORT,
-		max: 5,
-		idleTimeoutMillis: 10000,
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 	test: {
 		username: 'root',
