13d0001271704a4c474a0a28d866609ba170a54c
cinoxil <54507093+cinoxil@users.noreply.github.com>
2023-01-10 17:32:18 +0300

    Update useContact.js

--- frontend/src/hooks/useContact.js
+++ frontend/src/hooks/useContact.js
@@ -1,7 +1,7 @@
 import env from 'react-dotenv';
 export const useContact = () => {
 	const contact = async (vals) => {
-		const response = await fetch(`http://${env.MAIL_SERVICE_URL}/contact"`, {
+		const response = await fetch(`http://${env.MAIL_SERVICE_URL}/contact`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ ...vals }),
